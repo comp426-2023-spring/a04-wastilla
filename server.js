59e966751383141a273437d7b255486d2912c01f
wastilla <wastilla@email.unc.edu>
2023-04-24 12:50:24 -0400

    Baisc endpoints done

---
+++ server.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import express from "express"
+import minimist from "minimist"
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+let argv = minimist(process.argv.slice(2));
+const port = argv.port || 5000;
+
+//Begin listening and print name of port 
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
+
+
+app.get('/app', (req, res) => {
+    console.log(res.statusMessage)
+    res.status(200).send("200 OK");
+});
+
+
+// Default endpoint returns 404 error
+app.get('*', (req, res) => {
+    console.log(res.statusMessage)
+	res.status(400).send("404 NOT FOUND");
+});
+
+
+
+
