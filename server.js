7227fd59ceec87f9d3ea462430b8101b70e6f8db
wastilla <wastilla@email.unc.edu>
2023-04-24 13:02:14 -0400

    Implement /rps/ and /rpsls/ enpoints

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express"
 import minimist from "minimist"
 
 
+import {rps, rpsls} from './lib/rpsls.js';
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -16,11 +18,19 @@ app.listen(port, () => {
 });
 
 
-app.get('/app', (req, res) => {
+app.get('/app', (_, res) => {
     console.log(res.statusMessage)
     res.status(200).send("200 OK");
 });
 
+
+app.get('/app/rps/', (_, res) => {
+    res.status(200).send(rps());
+});
+
+app.get('/app/rpsls/', (_, res) => {
+	res.status(200).send(rpsls());
+});
 
 // Default endpoint returns 404 error
 app.get('*', (req, res) => {
